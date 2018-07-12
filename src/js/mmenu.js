bd3a5f49b80f5affad5eb93ed4292ab04b53a287
Olexandr Plakhotniuk <plaxotpos@gmail.com>
2018-07-13 01:26:53 +0300

    carousel for services and fotogallery have been finished

--- src/js/mmenu.js
+++ src/js/mmenu.js
@@ -11,7 +11,55 @@ $(document).ready(function() {
 		$('.hamburger').addClass('is-active');
 	});
 	api.bind('close:start', function() {
-		$('.hamburger').removeClass('is-active');
-	});
+        $('.hamburger').removeClass('is-active');
+    });
+
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        nav: true,
+        smartSpeed: 700,
+        navText: ['<i class="fa fa-angle-double-left"></i>', '<i class="fa fa-angle-double-right"></i>'],
+        responsiveClass: true,
+        dots: false,
+        responsive: {
+            0: {items: 1},
+            800: {items: 2},
+            1100: {items: 3}
+        }
+    });
+
+//height of elements
+    $('.carousel-service').on('initialized.owl.carousel', function(){
+
+    });
+    function carouselService(){
+		$('.carousel-service-item').each(function(){
+
+			$(this).find('.carousel-service-image').css('min-height', $(this).find('.carousel-service-content').outerHeight( true ));
+		});
+    };setTimeout(function(){
+        carouselService()
+    }, 500)
+
+//highlight last and first words for <span>
+    $('.carousel-service-composition h3').each(function(){
+		$(this).html($(this).html().replace(/(\S+)\s*$/, '<span>$1</span>'));
+    });
+    $('.s-king h2').each(function(){
+		$(this).html($(this).html().replace(/(\S+)/, '<span>$1</span>'));
+    });
+
+//Buttons 'prev' and 'next'
+    $('.owl-carousel').find('.owl-nav').removeClass('disabled');
+        $('.owl-carousel').on('changed.owl.carousel', function(event) {
+            $(this).find('.owl-nav').removeClass('disabled');
+     });
+
+
+//Resize Window
+$('.carousel-service-content').equalHeight({
+    updateOnDOMLoad: true,
+    //resizeTimeout: 40
+  })
 
 })
--- src/main.js
+++ src/main.js
@@ -9,6 +9,13 @@ require("../node_modules/font-awesome/css/font-awesome.min.css")
 require("../node_modules/hamburgers/dist/hamburgers.css")
 require("../node_modules/jquery.mmenu/dist/jquery.mmenu.all.js")
 require("../node_modules/jquery.mmenu/dist/jquery.mmenu.all.css")
+require("../node_modules/equal-height/dist/jquery.equalHeight.min.js")
+require("../node_modules/owl.carousel/dist/assets/owl.carousel.css")
+require("../node_modules/owl.carousel/dist/assets/owl.theme.default.min.css")
+require("../node_modules/owl.carousel/dist/owl.carousel.min.js")
+require("../node_modules/fotorama/fotorama.css")
+require("../node_modules/fotorama/fotorama.js")
+
 
 import $ from 'jquery';                 /* jquery in webpack 4 */
     import jQuery from 'jquery';
